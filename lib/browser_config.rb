6ba2b77781134783ea69766a2f2458d186fc722e
sreedev <sreedevpadmakumar@gmail.com>
2021-11-05 01:24:10 +0530

    browser config changes

--- lib/browser_config.rb
+++ lib/browser_config.rb
@@ -2,14 +2,9 @@
 
 # Bkmrq
 module Bkmrq
-  USER_HOME      = ENV.fetch('HOME', ::File.join('home', ENV['USER']))
+  USER_HOME             = ENV.fetch('HOME', ::File.join('home', ENV['USER']))
 
-  BROWSER_CONFIG = {
-    brave: BRAVE_BROWSER_CONFIG,
-    chrome: CHROME_BROWSER_CONFIG
-  }.freeze
-
-  BRAVE_BROWSER_CONFIG = {
+  BRAVE_BROWSER_CONFIG  = {
     bookmark_file_path: File.join(
       USER_HOME, '.config', 'BraveSoftware', 'Brave-Browser', 'Default', 'Bookmarks'
     ),
@@ -19,5 +14,10 @@ module Bkmrq
   CHROME_BROWSER_CONFIG = {
     bookmark_file_path: File.join(USER_HOME, '.config', 'chromium', 'Default', 'Bookmarks'),
     root: 'roots'
+  }.freeze
+
+  BROWSER_CONFIG = {
+    brave: BRAVE_BROWSER_CONFIG,
+    chrome: CHROME_BROWSER_CONFIG
   }.freeze
 end
